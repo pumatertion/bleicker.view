3e2026784c5d273f27ad675e10beb3a35f9f64b9
Carsten Bleicker <carsten@bleicker.de>
2015-04-30 17:59:04 +0200

    Introduce tests

---
+++ tests/Unit/ViewTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Bleicker\View\Unit;
+
+use Tests\Bleicker\View\UnitTestCase;
+
+/**
+ * Class ContentTest
+ *
+ * @package Tests\Bleicker\View\Unit
+ */
+class ContentTest extends UnitTestCase {
+
+	/**
+	 * @test
+	 */
+	public function resolveTest() {
+	}
+}
---
+++ tests/UnitTestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Bleicker\View;
+
+/**
+ * Class UnitTestCase
+ *
+ * @package Tests\Bleicker\View
+ */
+abstract class UnitTestCase extends BaseTestCase {
+
+}
